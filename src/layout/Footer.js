09092a654b9013942e826e6f2d8f3658f3ac5c15
Maneesh <maneeshsriram123@gmail.com>
2021-02-11 00:11:06 +0530

    Create Footer.js

---
+++ src/layout/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="bg-danger text-center text-white text-uppercase fixed-bottom pt-2">
+            <div className="container">
+                github search app with firebase
+            </div>
+            <p>By Maneesh</p>
+        </div>
+    )
+}
+
+export default Footer
