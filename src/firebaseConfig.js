d0fdaf761f5afa3ba4f08ef6a9a5afd031bd4aaa
Maneesh <maneeshsriram123@gmail.com>
2021-02-11 00:10:19 +0530

    Create firebaseConfig.js

---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export default {
+    apiKey: "AIzaSyDgjCgmm0kIOcVW8_zVPZB9ECbIUt5V9Wk",
+    authDomain: "mygitapp-a4f9d.firebaseapp.com",
+    databaseURL: "https://mygitapp-a4f9d.firebaseio.com",
+    projectId: "mygitapp-a4f9d",
+    storageBucket: "mygitapp-a4f9d.appspot.com",
+    messagingSenderId: "360818454296",
+    appId: "1:360818454296:web:07334ba130a7a0eaca786d",
+    measurementId: "G-LXYCB2L8L1"
+};
