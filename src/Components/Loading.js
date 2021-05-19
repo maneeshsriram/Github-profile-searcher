9f70eb04d3bf6797726858f08ae4fcdfe5edd54d
Maneesh <maneeshsriram123@gmail.com>
2021-05-19 09:17:01 +0530

    Create Loading.js

---
+++ src/Components/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Load from './loading.gif'
+
+const Loading = () => {
+    return (
+        <div className="mt-5 pt-5 text-center">
+            <img src={Load} alt="" />
+        </div>
+    )
+}
+
+export default Loading
