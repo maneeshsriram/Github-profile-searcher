bcc14f0cd39da71250dda99ce71c2ab8eb5239f1
Maneesh <maneeshsriram123@gmail.com>
2021-05-19 09:17:20 +0530

    Update Signin.js

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -5,19 +5,23 @@ import firebase from 'firebase/app'
 import { UserContext } from '../context/UserContext'
 import { Redirect } from 'react-router-dom'
 
+import Loading from '../Components/Loading'
+
 const Signup = () => {
     const context = useContext(UserContext)
-
+    const [load, setLoad] = useState(false)
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
     const handleSignup = () => {
+        setLoad(true)
         firebase
             .auth()
             .signInWithEmailAndPassword(email, password)
             .then(res => {
                 // console.log(res);
                 context.setUser({ email: res.user.email, uid: res.user.uid })
+                setLoad(false)
                 toast('Signed in successfully', {
                     type: 'success'
                 })
@@ -38,27 +42,41 @@ const Signup = () => {
     if (context.user?.uid) {
         return <Redirect to='/' />
     }
+
     return (
-        <div className="mt-5 pt-5">
-            <div className="col-md-6 offset-md-3">
-                <form onSubmit={handleSubmit} className="bg-light">
-                    <div className="card-header bg-info text-light">Signin</div>
-                    <div className="card-body border">
-                        <div class="form-group">
-                            <label for="exampleInputEmail1">Email address</label>
-                            <input className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" type="email" name='email' value={email} onChange={e => setEmail(e.target.value)} />
+        <div >
+            {
+                load ? (
+                    <Loading />
+                ) : (
+                    <div className="mt-5 pt-5">
+                        <div className="col-md-6 offset-md-3">
+                            <form onSubmit={handleSubmit} className="bg-light">
+                                <div className="card-header bg-info text-light">Signin</div>
+                                <div className="card-body border">
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Email address</label>
+                                        <input className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" type="email" name='email' value={email} onChange={e => setEmail(e.target.value)} />
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputPassword1">Password</label>
+                                        <input className="form-control" id="exampleInputPassword1" type="password" name='password' placeholder='Enter password' value={password} onChange={e => setPassword(e.target.value)} />
+                                    </div>
+                                    <button type="submit" className="btn btn-outline-primary btn-block">Login</button>
+                                </div>
+                            </form>
                         </div>
-                        <div class="form-group">
-                            <label for="exampleInputPassword1">Password</label>
-                            <input className="form-control" id="exampleInputPassword1" type="password" name='password' placeholder='Enter password' value={password} onChange={e => setPassword(e.target.value)} />
-                        </div>
-                        <button type="submit" className="btn btn-outline-primary btn-block">Login</button>
                     </div>
-                </form>
-            </div>
-        </div>
+                )
+            }
+        </ div>
+
     )
 }
+
+
+
+
 
 
 
